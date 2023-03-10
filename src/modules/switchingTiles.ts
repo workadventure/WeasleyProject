f0e59751fd2a077189dd4b8cba5a11b61b718fa2
morgane <m.huebra@thecodingmachine.com>
2023-03-10 17:32:08 +0100

    (Feat) switching tiles - Tiles change for every users

--- src/modules/switchingTiles.ts
+++ src/modules/switchingTiles.ts
@@ -6,14 +6,13 @@ const initiateSwitchingTiles = (switchingTiles: Array<string> = ['switchingTiles
     for (let j = 0; j < Object.keys(victoryCondition).length; j++) {
       console.log(`${switchingTiles[i]}/${j}_layer/zone`)
       WA.room.onEnterLayer(`${switchingTiles[i]}/${j}_layer/zone`).subscribe(() => {
-        console.log('entered layer')
         let newValue: number = WA.state[`${switchingTiles[i]}_${j}_value`] ? WA.state[`${switchingTiles[i]}_${j}_value`] as number : 0
         WA.state[`${switchingTiles[i]}_${j}_value`] = (newValue + 1) %  3 // TODO : store length in variable ?
+      })
 
-        // TODO : for every user ? --> see https://workadventu.re/map-building-extra/variable-to-property-binding.md
+      WA.state.onVariableChange(`${switchingTiles[i]}_${j}_value`).subscribe(() => {
         WA.room.hideLayer(`${switchingTiles[i]}/${j}_layer`)
         WA.room.showLayer(`${switchingTiles[i]}/${j}_layer/${WA.state[`${switchingTiles[i]}_${j}_value`]}`)
-        console.log('changed tile value to : ' + WA.state[`${switchingTiles[i]}_${j}_value`] )
       })
     }
   }
