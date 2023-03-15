c8e202290763356147d5b4d834f2243e115c8786
morgane <m.huebra@thecodingmachine.com>
2023-03-15 10:31:58 +0100

    (Feat) switching tiles - victory condition for all players

--- src/modules/switchingTiles.ts
+++ src/modules/switchingTiles.ts
@@ -1,21 +1,27 @@
-
-const initiateSwitchingTiles = (switchingTiles: Array<string> = ['switchingTiles']) => {
+const initiateSwitchingTiles = (switchingTiles: Array<string> = ['switchingTiles'], victoryCallBacks: Array<Function>|null = []) => {
   for (let i = 0; i < switchingTiles.length; i++) {
     const victoryCondition = JSON.parse(WA.state[`${switchingTiles[i]}VictoryCondition`] as string)
-    const tilesNumber = 3 // TODO :  from variable ?
+    const tilesNumber = WA.state[`${switchingTiles[i]}TilesNumber`] as number
 
     for (let j = 0; j < Object.keys(victoryCondition).length; j++) {
-      console.log(`${switchingTiles[i]}/${j}_layer/zone`)
       WA.room.onEnterLayer(`${switchingTiles[i]}/${j}_layer/zone`).subscribe(() => {
         let newValue: number = WA.state[`${switchingTiles[i]}_${j}_value`] ? WA.state[`${switchingTiles[i]}_${j}_value`] as number : 0
         WA.state[`${switchingTiles[i]}_${j}_value`] = (newValue + 1) %  tilesNumber
 
-        // TODO : Test victory
         if (testVictory(switchingTiles[i], victoryCondition)) {
-          console.log('VICTORY')
+          // set victory variable so that every user knows
+          WA.state[`${switchingTiles[i]}IsVictory`] = true
         }
       })
 
+      // Detect victory
+      WA.state.onVariableChange(`${switchingTiles[i]}IsVictory`).subscribe((value) => {
+        if (value && victoryCallBacks?.[i]) {
+          victoryCallBacks[i]()
+        }
+      })
+
+      // Change tiles for every user
       WA.state.onVariableChange(`${switchingTiles[i]}_${j}_value`).subscribe(() => {
         WA.room.hideLayer(`${switchingTiles[i]}/${j}_layer`)
         WA.room.showLayer(`${switchingTiles[i]}/${j}_layer/${WA.state[`${switchingTiles[i]}_${j}_value`]}`)
