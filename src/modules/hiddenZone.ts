fdf1248eb8f6541f20609a27daff8426a6a6811f
alixbTCM <a.boufflers@thecodingmachine.com>
2023-03-29 17:18:48 +0200

    add fog of war and more

--- src/modules/hiddenZone.ts
+++ src/modules/hiddenZone.ts
@@ -6,10 +6,12 @@ type hiddenZoneType = {
 const initiateHiddenZones = (zones: Array<hiddenZoneType>) => {
   for (let i = 0; i < zones.length; i++) {
     WA.room.onEnterLayer(zones[i].stepIn).subscribe(() => {
+      console.log('caché')
       WA.room.hideLayer(zones[i].hide)
     })
 
     WA.room.onLeaveLayer(zones[i].stepIn).subscribe(() => {
+      console.log('pas caché')
       WA.room.showLayer(zones[i].hide)
     })
   }
