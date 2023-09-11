6e66a3edc1ec277455f76223055e86ef1e3b6973
alixbTCM <a.boufflers@thecodingmachine.com>
2023-09-11 13:08:18 +0200

    all museum map

---
+++ src/translations/en-US/museum.js
@@ -0,0 +1,13 @@
+export default {
+    doorClosed : "This door seems to be locked",
+    search : "[TO RUMMAGE]",
+    idCardTitle: "Identity card",
+    idCardDescription : "The identity card of someone who looks really, really important",
+    searchEmpty: "Nothing interesting...",
+    pickpocket: "[PICKPOCKET]",
+    pickpocketEmpty: "mmmh... no, nothing interresting",
+    accessCard: "Golden key",
+    accessCardDescription: "Golden key who seems looks really important",
+    desktopOpenMsg: "*Clic clac* that's opened !",
+    desktopOpen: "[TRY THE GOLDEN KEY]",
+}
